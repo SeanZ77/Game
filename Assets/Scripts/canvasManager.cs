d50aebb94f0aaf41ff47aabd618bcddf23e5d179
Sean <seanzeng77@gmail.com>
2021-01-10 11:18:36 -0800

    low health
    added low health effect

--- Assets/Scripts/canvasManager.cs
+++ Assets/Scripts/canvasManager.cs
@@ -1,10 +1,17 @@
-﻿using System.Collections;
+﻿        using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class canvasManager : MonoBehaviour
 {
+    public GameObject FadePanel;
+
+    private void Start()
+    {
+        FadePanel.SetActive(false);
+    }
+
     public void QuitGame()
     {
         Application.Quit();
@@ -12,11 +19,26 @@ public class canvasManager : MonoBehaviour
 
     public void ResetLevel()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        StartCoroutine(FadeOutEffect(SceneManager.GetActiveScene().buildIndex));
+        GameObject.Find("PlayerInfo").GetComponent<PlayerInfo>().ResetValues();
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
     public void NextLevel()
     {
-        SceneManager.LoadScene((SceneManager.GetActiveScene().buildIndex+1)%SceneManager.sceneCountInBuildSettings);
+        StartCoroutine(FadeOutEffect(SceneManager.GetActiveScene().buildIndex + 1));
+        //SceneManager.LoadScene((SceneManager.GetActiveScene().buildIndex+1)%SceneManager.sceneCountInBuildSettings);
+    }
+
+    IEnumerator FadeOutEffect(int SceneToLoad)
+    {
+        FadePanel.SetActive(true);
+        for (int i = 0; i < 100; i++)
+        {
+            FadePanel.GetComponent<CanvasGroup>().alpha = (float)i * 0.01f;
+            yield return new WaitForSecondsRealtime(0.01f);
+        }
+
+        SceneManager.LoadScene(SceneToLoad);
     }
 }
--- Assets/Scripts/playerManager.cs
+++ Assets/Scripts/playerManager.cs
@@ -5,9 +5,17 @@ using UnityEngine.UI;
 
 public class playerManager : MonoBehaviour
 {
+    //ctrl + /
+    //cmd + /
+    //inventory management
+    //private List<Collectable> inventory = new List<Collectable>();
+    public Text inventoryText;
+    public Text descriptionText;
+    private int currentIndex;
+
     // Player specific variables
-    private int health;
-    private int score;
+    //private int health;
+    //private int score;
 
     // Boolean values
     private bool isGamePaused = false;
@@ -18,38 +26,98 @@ public class playerManager : MonoBehaviour
     public GameObject pauseMenu;
     public GameObject winMenu;
     public GameObject loseMenu;
+    public PlayerInfo info; //keep track of health, score, and inventory in this
+    public GameObject camera;
 
     // Start is called before the first frame update
     void Start()
-    {
+    {   
+
+        
+        if (info == null)
+        {
+            info = GameObject.Find("PlayerInfo").GetComponent<PlayerInfo>();
+        }
         // Makes sure game is "unpaused"
         isGamePaused = false;
         Time.timeScale = 1.0f;
 
         // Make sure all menus are filled in
         FindAllMenus();
+        camera.SetActive(false);
 
         //Start player with initial health and score
-        health = 100;
-        score = 0;
+        // info.health = 100;
+        // info.score = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        healthText.text = "Health: " + health.ToString();
-        scoreText.text  = "Score:  " + score.ToString();
+        if (info.inventory.Count == 0)
+        {
+            inventoryText.text = "Current Selection: None";
+            descriptionText.text = "";
+        }
+        else
+        {
+            inventoryText.text = "Current Selection: " + info.inventory[currentIndex].collectableName + " " + currentIndex.ToString();
+            descriptionText.text = "Press [E] to " + info.inventory[currentIndex].description;
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (info.inventory.Count > 0)
+            {
+                info.inventory[currentIndex].Use();
+                info.inventory.RemoveAt(currentIndex);
+                currentIndex = (currentIndex - 1) % info.inventory.Count;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            if (info.inventory.Count > 0)
+            {
+                currentIndex = (currentIndex + 1) % info.inventory.Count;
+            }
+        }
+
+        healthText.text = "Health: " + info.health.ToString();
+        scoreText.text = "Score:  " + info.score.ToString();
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             PauseGame();
         }
-        if (health <= 0)
+
+        if (info.health < 100)
+        {
+            info.health += 1;
+        }
+
+        if (info.health <= 40)
+        {
+            camera.SetActive(true);
+        }
+        if (info.health <= 0)
         {
             LoseGame();
         }
+
+        
     }
 
-   void FindAllMenus()
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Collectable>() != null)
+        {
+            collision.GetComponent<Collectable>().player = this.gameObject;
+            collision.gameObject.transform.parent = null;
+            info.inventory.Add(collision.GetComponent<Collectable>());
+            collision.gameObject.SetActive(false);
+        }
+    }
+
+    void FindAllMenus()
     {
         if (healthText == null)
         {
@@ -62,18 +130,21 @@ public class playerManager : MonoBehaviour
         if (winMenu == null)
         {
             winMenu = GameObject.Find("WinGameMenu");
-            winMenu.SetActive(false);
+
         }
+        winMenu.SetActive(false);
         if (loseMenu == null)
         {
             loseMenu = GameObject.Find("LoseGameMenu");
-            loseMenu.SetActive(false);
+
         }
+        loseMenu.SetActive(false);
         if (pauseMenu == null)
         {
             pauseMenu = GameObject.Find("PauseGameMenu");
-            pauseMenu.SetActive(false);
+
         }
+        pauseMenu.SetActive(false);
     }
 
     public void WinGame()
@@ -108,12 +179,19 @@ public class playerManager : MonoBehaviour
 
     public void ChangeHealth(int value)
     {
-        health += value;
+        info.health += value;
     }
 
     public void ChangeScore(int value)
     {
-        score += value;
+        info.score += value;
+    }
+
+    IEnumerator healthRegen()
+    {
+        info.health += 1;
+        yield return new WaitForSeconds(1f);
+
     }
 
 }
